24836e7394560929c708aad5dc06fbc413f44e77
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-07-14 00:42:36 +0100

    🚀 improve filter option persistance

--- src/features/expenses/FilterExpenses.tsx
+++ src/features/expenses/FilterExpenses.tsx
@@ -2,6 +2,8 @@ import {
   Button,
   DateRangePicker,
   DateRangePickerValue,
+  Select,
+  SelectItem,
   TextInput,
 } from '@tremor/react';
 import Label from '../../ui/Label';
@@ -10,30 +12,51 @@ import { useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 import { useSearchParams } from 'react-router-dom';
 import { formatDate } from '../../utils/helpers';
+import { categories } from '../../utils/constants';
+import CategoryIcon from '../../ui/CategorySelect';
 
 function FilterExpenses() {
   const [seachParams, setSearchParams] = useSearchParams();
-  const { register, watch, setValue, handleSubmit, control } = useForm({
+  const currentFrom = seachParams.get('from') || '';
+  const currentTo = seachParams.get('to') || '';
+
+  const currentDate: DateRangePickerValue = {
+    from: currentFrom ? new Date(currentFrom) : undefined,
+    to: currentTo ? new Date(currentTo) : undefined,
+  };
+  const currentSearch = seachParams.get('search') || '';
+  const currentMinAmount = seachParams.get('min-amount') || '';
+  const currentMaxAmount = seachParams.get('max-amount') || '';
+  const currentCategory = seachParams.get('category') || '';
+  const currentTag = seachParams.get('tag') || '';
+
+  const { register, watch, setValue, handleSubmit, reset, control } = useForm({
     defaultValues: {
-      date: {} as DateRangePickerValue,
-      search: '',
-      minAmount: '',
-      maxAmount: '',
-      category: '',
-      tag: '',
+      date: currentDate,
+      search: currentSearch,
+      minAmount: currentMinAmount,
+      maxAmount: currentMaxAmount,
+      category: currentCategory,
+      tag: currentTag,
     },
   });
 
+  const handleClearFilters = () => {
+    setSearchParams('');
+    reset();
+  };
+  console.log('re-rendered');
   const onSubmit = handleSubmit(data => {
     seachParams.set('from', formatDate(data.date.from ?? ''));
     seachParams.set('to', formatDate(data.date.to ?? ''));
-    seachParams.set('seacrh', data.search);
+    seachParams.set('search', data.search);
     seachParams.set('min-amount', data.minAmount);
     seachParams.set('max-amount', data.maxAmount);
-    seachParams.set('cat', data.category);
+    seachParams.set('category', data.category);
     seachParams.set('tag', data.tag);
 
     setSearchParams(seachParams);
+    console.log(data);
   });
 
   return (
@@ -44,14 +67,20 @@ function FilterExpenses() {
           <Button color="blue" size="xs">
             Filter
           </Button>
-          <Button variant="light" icon={HiXMark} size="xs">
+          <Button
+            type="button"
+            variant="light"
+            icon={HiXMark}
+            size="xs"
+            onClick={handleClearFilters}
+          >
             clear
           </Button>
         </div>
         <div>
           <Label>Date</Label>
           <DateRangePicker
-            value={watch('date')}
+            defaultValue={watch('date')}
             onValueChange={value => setValue('date', value)}
           />
         </div>
@@ -69,7 +98,24 @@ function FilterExpenses() {
         </div>
         <div>
           <Label>Category</Label>
-          <TextInput {...register('category')} />
+          <Select
+            id="category"
+            defaultValue={watch('category')}
+            onValueChange={category => {
+              console.log(category);
+              setValue('category', category);
+            }}
+          >
+            {categories.map(category => {
+              const CIcon = () => <CategoryIcon categoryKey={category.key} />;
+
+              return (
+                <SelectItem key={category.id} value={category.key} icon={CIcon}>
+                  {category.category}
+                </SelectItem>
+              );
+            })}
+          </Select>
         </div>
         <div>
           <Label>Tag</Label>
