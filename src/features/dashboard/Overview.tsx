8c2abf81a83560d9b8abdda08673b732d6c524e4
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-07-15 19:41:07 +0100

    🚀 add overview loading indicator

--- src/features/dashboard/Overview.tsx
+++ src/features/dashboard/Overview.tsx
@@ -3,6 +3,7 @@ import {
   Bold,
   Card,
   Flex,
+  Icon,
   Tab,
   TabGroup,
   TabList,
@@ -11,43 +12,66 @@ import {
 } from '@tremor/react';
 import { useCategoryOverview } from './useCategoryOverview';
 import { useState } from 'react';
+import {
+  FaArrowDown,
+  FaArrowUp,
+  FaCircleNotch,
+  FaSpinner,
+} from 'react-icons/fa';
 
 function Overview() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const selectedCategory = selectedIndex === 0 ? 'expenses' : 'incomes';
 
-  const { data } = useCategoryOverview();
+  const { data, isLoading } = useCategoryOverview();
 
   return (
     <Card>
       <Title>Overview</Title>
       <Text>Category</Text>
 
-      <TabGroup
-        index={selectedIndex}
-        onIndexChange={setSelectedIndex}
-        className="mt-6"
-      >
-        <TabList>
-          <Tab value="expenses">Expenses</Tab>
-          <Tab value="incomes">Incomes</Tab>
-        </TabList>
-      </TabGroup>
-      <Flex className="mt-6">
-        <Text>
-          <Bold>Category</Bold>
-        </Text>
-        <Text>
-          <Bold>Total</Bold>
-        </Text>
-      </Flex>
-      <BarList
-        // eslint-disable-next-line
-        //@ts-ignore
-        data={data ? data?.[selectedCategory] : []}
-        showAnimation={false}
-        className="mt-4"
-      />
+      {isLoading ? (
+        <div className="mt-20 text-center">
+          <Icon
+            icon={FaCircleNotch}
+            className="animate-spin"
+            size="xl"
+            color="gray"
+          />
+        </div>
+      ) : (
+        <>
+          <TabGroup
+            index={selectedIndex}
+            onIndexChange={setSelectedIndex}
+            className="mt-6"
+          >
+            <TabList>
+              <Tab value="expenses" icon={FaArrowUp}>
+                Expenses
+              </Tab>
+              <Tab value="incomes" icon={FaArrowDown}>
+                Incomes
+              </Tab>
+            </TabList>
+          </TabGroup>
+          <Flex className="mt-6">
+            <Text>
+              <Bold>Category</Bold>
+            </Text>
+            <Text>
+              <Bold>Total</Bold>
+            </Text>
+          </Flex>
+          <BarList
+            // eslint-disable-next-line
+            //@ts-ignore
+            data={data ? data?.[selectedCategory] : []}
+            showAnimation={false}
+            className="mt-4"
+          />
+        </>
+      )}
     </Card>
   );
 }
