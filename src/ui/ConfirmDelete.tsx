5fbd948b4e44e8e6a894270b5cbc4d01b71c5466
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-07-19 13:32:58 +0100

    💅 tweak confirm delete dialog

--- src/ui/ConfirmDelete.tsx
+++ src/ui/ConfirmDelete.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
-import * as AlertDialog from '@radix-ui/react-dialog';
-import { Button } from '@tremor/react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Button, Icon, Text } from '@tremor/react';
+import { FaExclamationCircle, FaExclamationTriangle } from 'react-icons/fa';
+import { HiXMark } from 'react-icons/hi2';
 
 type Props = {
   open: boolean;
@@ -17,36 +19,39 @@ const ConfirmDelete: FC<Props> = ({
   isLoading = false,
 }) => {
   return (
-    <AlertDialog.Root open={open} onOpenChange={setOpen}>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="fixed inset-0 bg-black/20" />
-        <AlertDialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <AlertDialog.Title className="mb-0 text-lg font-semibold">
-            Delete <span className="capitalize">{resource}</span>
-          </AlertDialog.Title>
-          <AlertDialog.Description className="mt-4 mb-5 leading-6 text-gray-500">
-            Are you sure you want to delete this {resource} permanently? This
-            action cannot be undone.
-          </AlertDialog.Description>
-          <div className="flex justify-end gap-6">
-            <AlertDialog.Close asChild>
-              <Button variant="light" color="gray" size="xs">
-                Cancel
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 duration-300 bg-black/20 animate-in fade-in-0" />
+        <Dialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Close className="absolute top-2 right-2">
+            <button>
+              <Icon icon={HiXMark} color="gray" />
+            </button>
+          </Dialog.Close>
+          <div className="text-center">
+            <Icon icon={FaExclamationCircle} size="lg" />
+          </div>
+          <Dialog.Description className="my-4 text-center">
+            <Text>Are you sure you want to delete this {resource} ?</Text>
+          </Dialog.Description>
+          <div className="flex justify-center gap-4">
+            <Dialog.Close asChild>
+              <Button color="gray" size="xs" variant="secondary">
+                No, Cancel
               </Button>
-            </AlertDialog.Close>
+            </Dialog.Close>
             <Button
               color="red"
-              variant="secondary"
               size="xs"
               onClick={onConfirm}
               loading={isLoading}
             >
-              Yes, delete <span className="capitalize">{resource}</span>
+              Yes, I'm sure
             </Button>
           </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
