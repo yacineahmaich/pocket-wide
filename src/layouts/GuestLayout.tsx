d90c3bbd49fb9866b5f0a27dd76a539ed5213e1b
yacineahmaich <yacineahmaich@gmail.com>
2023-09-21 19:27:28 +0100

    fix: landing page scroll issue

--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 
 function GuestLayout() {
   return (
-    <div className="bg-gray-50 bg-cover pt-16 sm:bg-[url(/images/landing-bg.jpg)] sm:bg-fixed">
+    <div className="overflow-hidden bg-gray-50 bg-cover pt-16 sm:bg-[url(/images/landing-bg.jpg)] sm:bg-fixed">
       <div className="container mx-auto px-3">
         <Header />
         <Outlet />
